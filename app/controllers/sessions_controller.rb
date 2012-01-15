2497949e03fc680aba1561d251995a9f2dd3bc98
Shanker Dhandapany <Shanker@Vasscon.com>
2012-01-14 20:50:00 -0800

    Done with sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+    @title = "Sign in"
+  end
+
+  def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination."
+      @title = "Sign in"
+      render 'new'
+    else
+      sign_in_user
+      redirect_to_user
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to_root_path
+  end
+
+end
